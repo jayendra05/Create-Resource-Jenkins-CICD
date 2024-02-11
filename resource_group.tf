a8d05c63b897383238a782bf8ba85ed4fcbde1f3
jayendra05 <42347169+jayendra05@users.noreply.github.com>
2024-02-12 00:42:51 +0530

    Second Commit

--- resource_group.tf
+++ resource_group.tf
@@ -1,18 +1,11 @@
 provider "azurerm" {
   features {}
-<<<<<<< HEAD
   tenant_id       = "735c2154-9cf0-4178-a7a8-00f406920e99"
   subscription_id = "23a55b34-95b8-42f4-a8cd-d4c734d74f2e"  
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "Genai-RG"
-=======
-
-}
 
 resource "azurerm_resource_group" "example" {
   name     = "Demo_RG"
->>>>>>> origin/main
   location = "East US"
 }
